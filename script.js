b42c49b4bc9fc27a5d6c5bf7315046153e4e47ad
Nova2836 <Troyhabib@gmail.com>
2023-10-31 18:35:56 -0700

    Update

---
+++ script.js
@@ -0,0 +1,27 @@
+let cube = document.querySelector('.obj');
+let moveBy = 10;
+
+window.addEventListener('load', () => {
+    cube.style.position = 'absolute';
+    cube.style.bottom = 0;
+    cube.style.right = 0;
+});
+window.addEventListener('keydown', (e) => {
+    switch(e.key){
+        case 'ArrowUp':
+            cube.style.bottom = parseInt(cube.style.bottom) + moveBy + 'px';
+            break;
+        
+        case 'ArrowDown':
+            cube.style.bottom = parseInt(cube.style.bottom) - moveBy + 'px';
+            break;
+        
+        case 'ArrowLeft':
+            cube.style.right = parseInt(cube.style.right) + moveBy + 'px';
+            break;
+                
+        case 'ArrowRight':
+            cube.style.right = parseInt(cube.style.right) - moveBy + 'px';
+            break;
+    }
+});
